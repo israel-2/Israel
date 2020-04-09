5fd26f36d971801037927061e9d9ccd61387b9bf
israel-2 <63377439+israel-2@users.noreply.github.com>
2020-04-09 00:01:19 -0500

    Update and rename sandbox to sandbox.c

---
+++ sandbox.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(void)
+{
+printf("hello,world\n");
+}
+
+
+
+*clang sandbox.c
+*./a.out
+to rename *clang -o output1 sandbox.c
+*./output1
+to see all my files *ls
+
+
+input
+to include all the other commands#include<cs50.h>
+just for print f#include<stdio.h>
+int main(void)
+{ 
+string answer=get_string("what's your name?\n");
+printf(hello, %s\n,answer);
+
+output 
+something redondon to inclde the cs50 *clang -o programm2 sandbox.c-lcs50
+or easy way and intelligent
+*make sandbox
+42 min
